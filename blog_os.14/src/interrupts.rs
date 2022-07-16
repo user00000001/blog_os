e4d9f161d06d9cbc0e56cc0865f01df1ed473cda
user00000001 <jaybrownjyb@gmail.com>
2022-07-16 11:44:17 +0800

    CPU Exceptions
    Signed-off-by: user00000001 <jaybrownjyb@gmail.com>

---
+++ blog_os.14/src/interrupts.rs
@@ -0,0 +1,23 @@
+use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+use crate::println;
+
+static mut IDT: InterruptDescriptorTable = InterruptDescriptorTable::new();
+
+pub fn init_idt() {
+    unsafe {
+        IDT.breakpoint.set_handler_fn(breakpoint_handler);
+        IDT.load();
+    }
+}
+
+extern "x86-interrupt" fn breakpoint_handler(
+    stack_frame: InterruptStackFrame)
+{
+    println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
+}
+
+#[test_case]
+fn test_breakpoint_exception() {
+    // invoke a breakpoint exception
+    x86_64::instructions::interrupts::int3();
+}
