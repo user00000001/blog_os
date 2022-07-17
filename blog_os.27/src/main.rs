85f8708ab8efe7dbe1fb26c6949e8a552a1fd19a
user00000001 <jaybrownjyb@gmail.com>
2022-07-17 10:36:49 +0800

    Heap Allocation
    Signed-off-by: user00000001 <jaybrownjyb@gmail.com>

--- blog_os.27/src/main.rs
+++ blog_os.27/src/main.rs
@@ -7,7 +7,7 @@
 
 extern crate alloc;
 
-use alloc::{boxed::Box, vec, vec::Vec, rc::Rc};
+use alloc::boxed::Box;
 
 use core::panic::PanicInfo;
 use bootloader::{BootInfo, entry_point};
@@ -18,39 +18,10 @@ mod serial;
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    use x86_64::VirtAddr;
-    use blog_os::allocator; // new import
-    use blog_os::memory::{self, BootInfoFrameAllocator};
     println!("Hello World{}", "!");
     blog_os::init();
 
-    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    let mut mapper = unsafe { memory::init(phys_mem_offset) };
-    let mut frame_allocator = unsafe {
-        BootInfoFrameAllocator::init(&boot_info.memory_map)
-    };
-
-    // new
-    allocator::init_heap(&mut mapper, &mut frame_allocator)
-        .expect("heap initialization failed");
-
-    // allocate a number on the heap
-    let heap_value = Box::new(41);
-    println!("heap_value at {:p}", heap_value);
-
-    // create a dynamically sized vector
-    let mut vec = Vec::new();
-    for i in 0..500 {
-        vec.push(i);
-    }
-    println!("vec at {:p}", vec.as_slice());
-
-    // create a reference counted vector -> will be freed when count reaches 0
-    let reference_counted = Rc::new(vec![1, 2, 3]);
-    let cloned_reference = reference_counted.clone();
-    println!("current reference count is {}", Rc::strong_count(&cloned_reference));
-    core::mem::drop(reference_counted);
-    println!("reference count is {} now", Rc::strong_count(&cloned_reference));
+    let x = Box::new(41);
     
     // as before
     #[cfg(test)]
